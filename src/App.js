ef631a17ff573f42600aa8b5d64e751d2cf9cba2
adanlessossi <adanlessossi@hotmail.com>
2019-06-10 11:32:22 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<p>Getting Docker to run!</p>
+			</header>
+			<p>Getting Docker to run Live reloading </p>
+		</div>
+	);
+}
+
+export default App;
